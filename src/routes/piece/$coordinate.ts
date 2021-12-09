cbd487fff1475f0970ac5eb8d78c003cdc9063e5
Josh McFarlin <josh@mcfarl.in>
2021-12-09 13:21:04 -0500

    Remove image caching

--- src/routes/piece/$coordinate.ts
+++ src/routes/piece/$coordinate.ts
@@ -29,7 +29,9 @@ export const loader: LoaderFunction = async ({
     headers: {
       "Content-Type": "image/png",
       "Content-Length": img.length.toString(),
-      "Cache-Control": "max-age=5",
+      "Cache-Control": "private, no-cache, no-store, must-revalidate",
+      Expires: "-1",
+      Pragma: "no-cache",
     },
   });
 };
